4f41749949d04f975fa76bbc637f22af3a970c00
visjha <vishaljha.cs14@gmail.com>
2020-04-30 04:30:18 -0700

    Controller Changes

--- src/main/java/com/coronaupdate/coronavirus/controller/HomeController.java
+++ src/main/java/com/coronaupdate/coronavirus/controller/HomeController.java
@@ -24,7 +24,6 @@ public class HomeController {
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
-
         return "home";
         }
 }
